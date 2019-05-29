02f4b0c692eb7a266aebd86d51e365c8036dd39c
shicks1080 <stuart.hicks@gmail.com>
2019-05-29 19:52:46 +0200

    Close of play 29/5/19
    Adding content to interests and skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,5 +5,20 @@ import Layout from "../components/layout"
 export default () => (
   <Layout pageTitle="Skills">
     <h1>Skills</h1>
+    <h3><i>Technical</i></h3>
+    <ul>
+      <li>Mobile: Android - </li>
+      <li>Mobile: iOs - </li>
+      <li>Mobile: React Native - </li>
+      <li>Responsive PWAs: Gatsby/React - </li>
+      <li>Version Control: Git</li>
+    </ul>
+    <h3><i>Soft</i></h3>
+    <ul>
+      <li>Teamwork - </li>
+      <li>Independant - </li>
+      <li>Detective - </li>
+      <li></li>
+    </ul>
   </Layout>
 )
