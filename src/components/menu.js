1bd6e4716adfa61d5433eee0775f933895ff3f4a
shicks1080 <stuart.hicks@gmail.com>
2019-05-29 14:58:47 +0200

    Customising the menu
    Colours and hamburger.

---
+++ src/components/menu.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { Nav, Navbar } from "react-bootstrap"
+
+import SocialMediaLinks from "./socialmedialinks"
+import styles from "./menu.module.css"
+
+export default (props) => (
+  <Navbar expand="sm">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Brand className="actionBar">{props.pageTitle}</Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="justify-content-center">
+        <Nav.Item>
+          <Nav.Link
+            className={styles.menuBar}
+            href="/"
+          >
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            className={styles.menuBar}
+            href="/experience/"
+          >
+            Experience
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            className={styles.menuBar}
+            href="/portfolio"
+          >
+            Portfolio
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            className={styles.menuBar}
+            href="/skills/"
+          >
+            Skills
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/bio/">Bio</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar.Collapse>
+    <SocialMediaLinks />
+  </Navbar>
+)
