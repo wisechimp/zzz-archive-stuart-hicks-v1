a4a4be1d6f6e718cd5c4472dab997c24c6e7b46e
shicks1080 <stuart.hicks@gmail.com>
2019-05-29 14:56:29 +0200

    Social media component for the menu
    Note the ridiculous line height which allows this component to align with the rest of the menu.

---
+++ src/components/socialmedialinks.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa"
+
+import styles from "./socialmedialinks.module.css"
+
+export default () => (
+  <div className={styles.socialGroup}>
+    <a
+      href="https://www.linkedin.com/in/stuarthicksse"
+      aria-label="LinkedIn icon link to Stuart Hicks' LinkedIn page"
+    >
+      <FaLinkedin />
+    </a>
+    <a
+      href="https://github.com/wisechimp"
+      aria-label="github icon link to Stuart Hicks' github page which is under the wise chimp pseudonym"
+    >
+      <FaGithubSquare />
+    </a>
+  </div>
+)
