d033234680c029b6539c72e5549422bc3832e8c2
wisechimp <stuart.hicks@gmail.com>
2019-10-14 16:04:04 +0200

    Adding button to portfolio card

--- src/components/portfoliocard.js
+++ src/components/portfoliocard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 import styles from "./portfoliocard.module.css"
@@ -11,5 +12,8 @@ export default(props) => (
     <p className={styles.portfolioCardPlatform}>{props.platform}</p>
     <Img className={styles.portfolioCardImage} fluid={props.projectScreenshot} alt={props.imgAltText}/>
     <p className={styles.portfolioCardText}>{props.description}</p>
+    <div className={styles.portfolioCardButt}>
+      <Link className={styles.portfolioCardButtText} to={props.hyperlink}>Show Me More!</Link>
+    </div>
   </div>
 )
